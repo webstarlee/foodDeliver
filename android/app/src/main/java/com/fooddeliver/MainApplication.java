bd8550475850f6f502443de2702a2736b2539ff8
webdev55486 <chol.r@hotmail.com>
2018-05-24 13:49:56 +0800

    not yet

--- android/app/src/main/java/com/fooddeliver/MainApplication.java
+++ android/app/src/main/java/com/fooddeliver/MainApplication.java
@@ -3,8 +3,8 @@ package com.fooddeliver;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new LinearGradientPackage()
+            new LinearGradientPackage(),
+            new VectorIconsPackage()
       );
     }
 
--- ios/Pods/Target Support Files/Pods-fooddeliver/Pods-fooddeliver-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_fooddeliver : NSObject
-@end
-@implementation PodsDummy_Pods_fooddeliver
-@end
